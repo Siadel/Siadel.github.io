4dcf11d7fdd962f72100fb663cbb55706316c25c
Siadel <ghftrp908334@naver.com>
2026-05-07 11:10:52 +0900

    -- svelte.config.js의 kit.paths.base 수정

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/Siadel.github.io' : ''
+			base: process.env.NODE_ENV === 'production' ? '/siadel.github.io' : ''
 		}
 	},
 	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.svx', '.md'] })],
